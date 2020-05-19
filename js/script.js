7563650426280b4c7de241f011f68694253f4016
farhanxavio <farhanxavio@gmail.com>
2020-05-19 23:06:39 +0530

    add go-to-top btn

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@ $(function(){
     $(document).scroll(function(){
         var $nav = $('#main-navbar')
         $nav.toggleClass('scrolled',$(this).scrollTop() > $nav.height())
+        $('#to-top').toggleClass('display',$(this).scrollTop() > $nav.height())
     })
 })
 
 $('.navbar-toggler').on('click',() => {
     $('#main-navbar').toggleClass('pressed')
 })
+
+$("#to-top").click(function () {
+    //1 second of animation time
+    //html works for FFX but not Chrome
+    //body works for Chrome but not FFX
+    //This strange selector seems to work universally
+    $("html, body").animate({scrollTop: 0}, 100);
+ });
