38585c0652d499d118735c44a8326f079d6acd91
farhanxavio <farhanxavio@gmail.com>
2020-05-21 16:13:25 +0530

    add active class on scroll

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
+
 $(function(){
     $(document).scroll(function(){
         var $nav = $('#main-navbar')
         $nav.toggleClass('scrolled',$(this).scrollTop() > $nav.height())
         $('#to-top').toggleClass('display',$(this).scrollTop() > $nav.height())
+
+        // Add active class on scroll
+        var scrollPos = $(document).scrollTop();
+        $('#main-navbar ul li a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#main-navbar ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
     })
 })
 
@@ -11,9 +26,6 @@ $('.navbar-toggler').on('click',() => {
 })
 
 $("#to-top").click(function () {
-    //1 second of animation time
-    //html works for FFX but not Chrome
-    //body works for Chrome but not FFX
-    //This strange selector seems to work universally
     $("html, body").animate({scrollTop: 0}, 100);
  });
+
